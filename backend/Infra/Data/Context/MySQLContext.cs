ee20364b2a27fb7aa2d81baca488a6ac70f46350
Gilcelino Costa <gilcelino@gmail.com>
2022-02-12 17:49:16 -0300

    Created migration

--- backend/Infra/Data/Context/MySQLContext.cs
+++ backend/Infra/Data/Context/MySQLContext.cs
@@ -6,9 +6,21 @@ namespace Data.Context
 {
     public class MySQLContext : DbContext
     {
+        public MySQLContext()
+        {
+            
+        }
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){}
         public DbSet<User> Users {get; set;}
-
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string sConnection = "Server=localhost;Port=3306;Database=news;Uid=root;Pwd=root";
+            
+            optionsBuilder.UseMySql(sConnection
+                , ServerVersion.AutoDetect(sConnection), null );
+        }
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
---
+++ backend/Infra/Data/Migrations/20220212203414_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace News.Infra.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INT(10)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "VARCHAR(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "VARCHAR(200)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "VARCHAR(100)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
